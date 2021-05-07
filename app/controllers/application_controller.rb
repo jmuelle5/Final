76d5e9e48724dc6df778b6a905a3251845683a1b
Jacob <jmuelle5@uccs.edu>
2021-05-07 19:55:21 +0000

    Version 1.0.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    ActiveRecord::Migration.maintain_test_schema!
     before_action :authenticate_user!, except: [:welcome, :about]
     before_action :authenticate_user!
 end
